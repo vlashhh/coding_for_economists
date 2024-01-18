6334a75f9a293589107fe18c11838b154438bca3
vlashhh <shatl440@gmail.com>
2024-01-18 13:22:28 +0000

    Added first two intro to python scripts

---
+++ 02_basic_python.py
@@ -0,0 +1,33 @@
+print(2+4)
+
+print(10/5)
+
+print(type(10 / 5))
+
+print(2 ** 3)
+
+x = 5 
+y = x**2
+print(y)
+
+a = "Hello how're you?" 
+
+print(a*5)
+
+b = "I'm good, thank you."
+print(a + " " + b)
+
+#print(a / b)
+
+print(a[0])
+print(a[0:5])
+
+print(f'Our string a has {len(a)} characters')
+
+print("Our string a has {} characters".format(len(a)))
+
+print(2 == 2)
+
+print(a == b)
+
+print(2 == 2 and 3 == 3)
